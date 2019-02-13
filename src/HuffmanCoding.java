a114db0c558488170afb299dd09542a0fc6a4a23
ProfFunami <nishiyama.daiki.tu@alumni.tsukuba.ac.jp>
2019-02-13 18:22:37 +0900

    updata for moving

--- src/HuffmanCoding.java
+++ src/HuffmanCoding.java
@@ -2,27 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class HuffmanCoding {
-    private static double entropy = 0.0;
     private static double acwl = 0.0;
     private static ArrayList<Node> hnodes = new ArrayList<Node>();
 
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<Node> tree = new ArrayList<Node>();
 
-        File inputFile = new File("ea.txt");
+        File inputFile = new File("dict_probability-of-occurrence.txt");
         Scanner inFile = new Scanner(inputFile);
         inFile.useDelimiter("[^A-za-z0-9 ']+");
 
         while (inFile.hasNextLine()) {
             String nextLine = inFile.nextLine();
             Scanner SandPScanner = new Scanner(nextLine);
-            SandPScanner.useDelimiter(",");
+            SandPScanner.useDelimiter(" ");
             String c = SandPScanner.next();
             String pString = SandPScanner.next();
             double p = Double.parseDouble(pString);
             Node hn = new Node(c, p, null, null);
             tree.add(hn);
-            entropy -= p * Math.log(p) / Math.log(2.0);
             SandPScanner.close();
         }
         inFile.close();
@@ -35,17 +33,14 @@ public class HuffmanCoding {
             tree.remove(hn2);
             Node node = new Node("blank", hn1.probability + hn2.probability, hn1, hn2);
             tree.add(node);
-         /*for(Node n : tree)
-            System.out.println(n.symbol + ", " + n.probability);
-         System.out.println("-------------------");*/
         }
 
         encode(tree.get(0));
         displayTree(tree.get(0));
         for (Node hn : hnodes)
-            System.out.println(hn.symbol + ", " + hn.code);
+            System.out.println(hn.symbol + ": " + hn.code);
         System.out.println();
-        System.out.println("Entropy: " + entropy + ", " + "ACWL: " + acwl);
+        System.out.println("平均符号語長: " + acwl);
 
     }
 
